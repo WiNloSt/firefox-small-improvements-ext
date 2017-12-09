cb025d5563ebd8efcbbb90183d14db743d82037d
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-12-09 13:35:42 +0700

    Add text box after every rated questions

--- src/addLayout.js
+++ src/addLayout.js
@@ -8,8 +8,7 @@ var config = { childList: true }
 var callback = function(mutationsList) {
   for (var mutation of mutationsList) {
     if (mutation.type == 'childList') {
-      console.log(document.querySelector('feedback-provide'))
-      console.log(document.querySelectorAll('.ql-editor'))
+      debug()
     }
   }
 }
@@ -18,3 +17,18 @@ var observer = new MutationObserver(callback)
 
 observer.observe(targetNode, config)
 console.log('done!!!')
+
+function debug() {
+  const ratedQuestions = Array.from(
+    document.querySelectorAll('.feedback-provide-sections-section')
+  ).filter(el => el.querySelector('section-rated-question'))
+
+  ratedQuestions.forEach(reatedQuestion => {
+    const textArea = document.createElement('textarea')
+    textArea.style.cssText = 'width:100%; height:6em;'
+    reatedQuestion.appendChild(textArea)
+  })
+}
+debug()
+
+// console.log(document.querySelectorAll('.ql-editor'))
